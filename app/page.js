db72e4fbd7d19d8842edab0670b36e5bba45d1aa
manueljo <mikebillyy@gmail.com>
2025-08-05 21:50:48 +0100

    Add About section

--- app/page.js
+++ app/page.js
@@ -1,20 +1,24 @@
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 import Image from "next/image";
 import hero from '../public/images/hero.png';
+import About from "@/components/about";
 
 export default function Home() {
   return (
+    <>
     <BackgroundBeamsWithCollision className={'h-auto pt-24 px-4'}>
       <div className="h-full max-w-[1024px] w-full mx-auto px-4 flex items-center justify-between flex-wrap">
         <div>
           <h6 className="text-3xl mb-5">Hi There, I&apos;m</h6>
-          <h1 className="text-6xl font-bold mb-5">Emmanuel<br/>Joshua</h1>
+          <h1 className="text-6xl font-bold mb-5">Emmanuel<br />Joshua</h1>
           <h3 className="text-4xl  mb-8">A Software Engineer.</h3>
         </div>
         <div className="relative w-[300px] self-end flex flex-col justify-end">
-          <Image src={hero} alt="Portfolio hero image" objectFit="contain" className=""/>
+          <Image src={hero} alt="Portfolio hero image" objectFit="contain" className="" />
         </div>
       </div>
     </BackgroundBeamsWithCollision>
+    <About />
+    </>
   );
 }
---
+++ components/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section className='py-20 px- max-w-[1024px] mx-auto px-4'>
+        <h2 className='color-foreground text-3xl mb-3'>About</h2>
+        <p className='color-foreground'>
+            I'm a full-stack web developer based in Nigeria, focused on building real-world products with Django, React, and modern tools like Tailwind and Next.js. I have a background in IT systems and networking, which gives me a strong foundation in building secure, scalable applications. I'm always learning and experimenting...
+        </p>
+    </section>
+  )
+}
